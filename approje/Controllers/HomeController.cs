44528d44cef3dc6595e0ed4af3f03092db98abb8
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-06-15 12:02:54 +0400

    Add project files.

---
+++ approje/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using approje.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace approje.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult birthday()
+        {
+            return View();
+        }
+
+        public IActionResult events()
+        {
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
