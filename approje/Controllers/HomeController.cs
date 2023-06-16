fb72890500939192506ef3dfed6a982b67d80f81
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-06-17 01:52:28 +0400

    son

--- approje/Controllers/HomeController.cs
+++ approje/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using approje.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace approje.Controllers
 {
@@ -44,6 +45,66 @@ namespace approje.Controllers
         {
             return View();
         }
+        [Route("/home/help-and-support")]
+        public IActionResult help_and_support()
+        {
+            return View("help-and-support");
+        }
+        
+        [Route("/home/live-chat")]
+        public IActionResult live_chat()
+        {
+            // Gerekli işlemler
+            return View("live-chat");
+        }
+        public IActionResult marketplace()
+        {
+            return View();
+        }
+        public IActionResult messages()
+        {
+            return View();
+        }
+        [Route("/home/my-profile")]
+        public IActionResult my_profile()
+        {
+            return View("my-profile");
+        }
+
+        public IActionResult notifications()
+        {
+            return View();
+        }
+        public IActionResult register() => View();
+
+        public IActionResult setting()
+        {
+            return View();
+        }
+
+        public IActionResult video()
+        {
+            return View();
+        }
+
+        public IActionResult weather()
+        {
+            return View();
+        }
+
+        [Route("/home/forgot-password")]
+        public IActionResult forgot_password() 
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
